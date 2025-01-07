309e87f0698210d314a3042cb2ac36f9a76b70a2
JJ <joshua.james918@gmail.com>
2025-01-06 21:28:09 -0600

    added var, arrays, and function when page is called

---
+++ Main/Assets/JavaScript/script.js
@@ -0,0 +1,26 @@
+var wordBlank = document.querySelector(".word-blanks");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button")
+
+var chosenWord = "";
+var numBlanks = 0;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
+
+// Arrays used to create blanks and letters of screen
+var letterInChosenWord = [];
+var blankLetters = [];
+
+// Array of words the user will guess
+var words = ["Joshua", "Ronald", "Wilson", "Nitin", "Adam", "Blair", "Casey", "Julian", "Frank" ]
+
+// The Init function is called when the page loads
+function init() {
+    getwins();
+    getLosses();
+}
